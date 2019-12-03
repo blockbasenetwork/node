5a3792a0d720d8604320fc4acc45ae29a46cdf3b
Ricardo Alexandre Schiller Pinto <ricardoschiller@users.noreply.github.com>
2019-12-03 17:27:25 +0000

    deleted lots of old code related to the old way of query building.

--- BlockBase.Domain/Configurations/NodeConfigurations.cs
+++ BlockBase.Domain/Configurations/NodeConfigurations.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace BlockBase.Domain.Configurations
 {
     public class NodeConfigurations
@@ -10,10 +6,6 @@ namespace BlockBase.Domain.Configurations
         public string ActivePrivateKey { get; set; }
         public string ActivePublicKey { get; set; }
         public string SecretPassword { get; set; }
-        public string MySqlServer { get; set; }
-        public int MySqlPort { get; set; }
-        public string MySqlUser { get; set; }
-        public string MySqlPassword { get; set; }
         public string MongoDbConnectionString { get; set; }
         public string MongoDbPrefix { get; set; }
     }
