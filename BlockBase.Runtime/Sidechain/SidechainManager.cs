a35bcc6142f24f9513f8d47b0d38d39537356f94
dbulha <49444700+dbulha@users.noreply.github.com>
2020-04-24 11:47:30 +0100

    Change action order

--- BlockBase.Runtime/Sidechain/SidechainManager.cs
+++ BlockBase.Runtime/Sidechain/SidechainManager.cs
@@ -560,8 +560,8 @@ namespace BlockBase.Runtime.Sidechain
             var rewardTable = await _mainchainService.RetrieveRewardTable(_nodeConfigurations.AccountName);
             if (rewardTable.Any(r => r.Reward > 0 && r.Key == Sidechain.ClientAccountName))
             {
-                await HistoryValidationHelper.CheckSidechainValidationProposal(_mainchainService, _nodeConfigurations.AccountName, Sidechain.ClientAccountName, _logger);
                 await _mainchainService.ClaimReward(Sidechain.ClientAccountName, _nodeConfigurations.AccountName);
+                await HistoryValidationHelper.CheckSidechainValidationProposal(_mainchainService, _nodeConfigurations.AccountName, Sidechain.ClientAccountName, _logger);
             }
         }
 
