7e1b412de5b0c2d9ff6f0295012a5a4a20634864
Ricardo Schiller <ricardoschiller@users.noreply.github.com>
2020-05-25 15:15:01 +0100

    Changed how the SecretPassword is used and generated.

--- BlockBase.Domain/Configurations/NodeConfigurations.cs
+++ BlockBase.Domain/Configurations/NodeConfigurations.cs
@@ -1,11 +1,17 @@
+using System;
+
 namespace BlockBase.Domain.Configurations
 {
     public class NodeConfigurations
     {
+        private string _secretPassword = Guid.NewGuid().ToString();
         public string AccountName { get; set; }
         public string ActivePrivateKey { get; set; }
         public string ActivePublicKey { get; set; }
-        public string SecretPassword { get; set; }
+        public string SecretPassword 
+        { 
+            get { return _secretPassword; }
+        }
         public string MongoDbConnectionString { get; set; }
         public string MongoDbPrefix { get; set; }
         public string PostgresHost { get; set; }
