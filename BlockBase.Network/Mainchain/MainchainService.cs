14de6ac6a52a107e87ddbd7fbc1eda211120b001
dbulha <49444700+dbulha@users.noreply.github.com>
2020-06-01 15:02:37 +0100

    Change send transaction error  log levels

--- BlockBase.Network/Mainchain/MainchainService.cs
+++ BlockBase.Network/Mainchain/MainchainService.cs
@@ -824,11 +824,11 @@ namespace BlockBase.Network.Mainchain
             }
 
             var errorMessage = exception is ApiErrorException apiException ?
-                        $"Error sending transaction: {apiException.error.name} Trace: {exception}" :
-                        $"Error sending transaction: {exception}";
+                        $"Error sending transaction: {apiException.error.name} Message: {apiException.message}" :
+                        $"Error sending transaction";
 
             _logger.LogCritical(errorMessage);
-
+            _logger.LogDebug($"Send Transaction Error Trace: {exception}");
             throw exception;
         }
 
