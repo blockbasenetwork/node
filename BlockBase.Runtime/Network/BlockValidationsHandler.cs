8600b2110e451b4cbe8d4cb7876271b6041bc60a
dbulha <49444700+dbulha@users.noreply.github.com>
2020-06-15 16:18:36 +0100

    Remove incorrect validation

--- BlockBase.Runtime/Network/BlockValidationsHandler.cs
+++ BlockBase.Runtime/Network/BlockValidationsHandler.cs
@@ -66,9 +66,6 @@ namespace BlockBase.Runtime.Network
             await sidechainSemaphore.WaitAsync();
             try
             {
-                if(!ValidateBlockSize(blockProtoReceived))
-                    throw new Exception("Block size incorrect.");
-            
                 var blockReceived = new Block().SetValuesFromProto(blockProtoReceived);
                 
                 var blockHashString = HashHelper.ByteArrayToFormattedHexaString(blockReceived.BlockHeader.BlockHash);
@@ -120,18 +117,6 @@ namespace BlockBase.Runtime.Network
                 sidechainSemaphore.Release();
             }
 
-        }
-
-        private bool ValidateBlockSize(BlockProto blockProtoReceived)
-        {
-            var declaredBlockSize =  blockProtoReceived.BlockHeader.BlockSizeInBytes;
-            blockProtoReceived.BlockHeader.BlockSizeInBytes = 0;
-            blockProtoReceived.BlockHeader.BlockHash = null;
-            blockProtoReceived.BlockHeader.ProducerSignature = null;
-            var blockSize = (ulong) blockProtoReceived.ToByteArray().Count();
-            blockProtoReceived.BlockHeader.BlockSizeInBytes = declaredBlockSize;
-
-            return declaredBlockSize == blockSize;
         }
 
         private async Task<bool> AlreadyProcessedThisBlock(string sidechain, string blockhash)
