3bf9ce9c911b01670265c19c0f0b398b5e14aeab
dbulha <49444700+dbulha@users.noreply.github.com>
2020-06-22 12:36:29 +0100

    Update history validation table

--- BlockBase.Network/Mainchain/Pocos/HistoryValidationTable.cs
+++ BlockBase.Network/Mainchain/Pocos/HistoryValidationTable.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using BlockBase.Domain.Eos;
 using Newtonsoft.Json;
 
@@ -10,6 +11,12 @@ namespace BlockBase.Network.Mainchain.Pocos
 
         [JsonProperty(EosAtributeNames.BLOCK_HASH)]
         public string BlockHash { get; set; }
+
+        [JsonProperty(EosAtributeNames.VERIFY_SIGNATURES)]
+        public List<string> VerifySignatures {get; set;}
+
+        [JsonProperty(EosAtributeNames.PACKED_TRANSACTION)]
+        public string ValidateHistoryPackedTransaction { get; set; }
 
         [JsonProperty(EosAtributeNames.BLOCK_BYTE_IN_HEXADECIMAL)]
         public string BlockByteInHexadecimal { get; set; }
