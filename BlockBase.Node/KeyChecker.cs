453eb4dcd28e2f6d49c4a3bc074ee24176f730d1
dbulha <49444700+dbulha@users.noreply.github.com>
2020-07-13 11:21:20 +0100

    Remove unnecessary using

--- BlockBase.Node/KeyChecker.cs
+++ BlockBase.Node/KeyChecker.cs
@@ -1,22 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
-using BlockBase.DataPersistence.Data;
 using BlockBase.Domain.Configurations;
-using BlockBase.Domain.Endpoints;
-using BlockBase.Domain.Enums;
-using BlockBase.Domain.Eos;
-using BlockBase.Domain.Results;
 using BlockBase.Network.Mainchain;
-using BlockBase.Network.Mainchain.Pocos;
-using BlockBase.Domain.Blockchain;
-using BlockBase.Utils;
-using BlockBase.Utils.Threading;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using EosSharp.Core.Helpers;
 using Cryptography.ECDSA;
 using EosSharp.Core.Exceptions;
@@ -47,7 +34,7 @@ namespace BlockBase.Node
         {
             var privateKeyBytes = CryptoHelper.GetPrivateKeyBytesWithoutCheckSum(_nodeConfigurations.ActivePrivateKey);
             var publicKey = CryptoHelper.PubKeyBytesToString(Secp256K1Manager.GetPublicKey(privateKeyBytes, true));
-            
+
             if (publicKey != _nodeConfigurations.ActivePublicKey)
             {
                 _logger.LogCritical($"The configured key private key {_nodeConfigurations.ActivePrivateKey} doesn't match the configured public key {_nodeConfigurations.ActivePublicKey}");
