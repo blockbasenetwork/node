e0e5ab53078300ab74234c7f428284d5f32a9006
Márcia Policarpo <m.f.krus@hotmail.com>
2020-07-14 17:27:06 +0100

    Fixed bug.

--- BlockBase.Runtime/Common/ProviderAbstractState.cs
+++ BlockBase.Runtime/Common/ProviderAbstractState.cs
@@ -21,10 +21,17 @@ namespace BlockBase.Runtime.Common
             _mainchainService = mainchainService;
         }
 
-        public override async  Task<string> Run(CancellationToken cancellationToken)
+        public override async Task<string> Run(CancellationToken cancellationToken)
         {
             _logger.LogDebug($"{Path} - Checking if sidechain is still the same...");
-            if(!await IsSidechainStillTheSame()) throw new Exception("Sidechain is no longer the same.");
+            if (!await IsSidechainStillTheSame())
+            {
+
+                _logger.LogDebug($"{Path} - Sidechain is no longer the same.");
+                //if there are no conditions to continue on the start state, jump to the end state
+                if (this is TStartState) return typeof(TEndState).Name;
+                return typeof(TStartState).Name; //returns control to the State Manager indicating same state
+            }
             _logger.LogInformation($"{Path} - Sidechain checked");
             return await base.Run(cancellationToken);
 
