1729a9e5a13e3e4c5f984a32412f13b3c5ae3502
dbulha <49444700+dbulha@users.noreply.github.com>
2020-07-21 17:24:07 +0100

    Continue execution of endstate when sidechain changes

--- BlockBase.Runtime/Common/ProviderAbstractState.cs
+++ BlockBase.Runtime/Common/ProviderAbstractState.cs
@@ -29,9 +29,8 @@ namespace BlockBase.Runtime.Common
             {
 
                 _logger.LogDebug($"{Path} - Sidechain is no longer the same.");
-                //if there are no conditions to continue on the start state, jump to the end state
-                if (this is TStartState) return typeof(TEndState).Name;
-                return typeof(TStartState).Name; //returns control to the State Manager indicating same state
+                //if there are no conditions to continue on this sidechain, jump to the end state
+                if (!(this is TEndState)) return typeof(TEndState).Name;
             }
             _logger.LogInformation($"{Path} - Sidechain checked");
             return await base.Run(cancellationToken);
