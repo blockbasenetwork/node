ce2b5a4f650f9c70cf0e22d3fdccac1137647a94
dbulha <49444700+dbulha@users.noreply.github.com>
2020-08-07 08:59:46 +0100

    Revert "Revert "Added column info to get structure.""
    This reverts commit 8208ec45a93d281f006bce39712018efce4d972b.

--- BlockBase.Domain/Database/Sql/QueryBuilder/Elements/Common/ColumnConstraint.cs
+++ BlockBase.Domain/Database/Sql/QueryBuilder/Elements/Common/ColumnConstraint.cs
@@ -17,6 +17,24 @@
             ForeignKey
         }
 
+        public override string ToString()
+        {
+            string str = "";
+            if(Name != null) str += Name.Value + " ";
+
+            str += ColumnConstraintType;
+            bool first = true;
+            if(ForeignKeyClause != null){
+            foreach(var column in ForeignKeyClause.ColumnNames)
+            {
+                if (first) str += " ";
+                else str += ", ";
+                str += ForeignKeyClause.TableName.Value + "." + column.Value;
+            }
+            }            
+            return str;
+        }
+
         public ColumnConstraint Clone()
         {
             return new ColumnConstraint() { Name = Name.Clone(), ColumnConstraintType = ColumnConstraintType, ForeignKeyClause = ForeignKeyClause.Clone() };
--- BlockBase.Domain/Pocos/FieldPoco.cs
+++ BlockBase.Domain/Pocos/FieldPoco.cs
@@ -8,9 +8,9 @@ public class FieldPoco
     {
         public string Name { get; private set; }
         public string Type { get; private set; }
-        public JArray Data { get; private set; }
+        public IList<string> Data { get; private set; }
 
-        public FieldPoco(string name, string type, JArray data)
+        public FieldPoco(string name, string type, IList<string> data)
         {
             Name = name;
             Type = type;
