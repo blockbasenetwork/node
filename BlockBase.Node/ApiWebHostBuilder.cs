eae86ee4d70eb52b47d641ec0d03f8725b178637
dbulha <49444700+dbulha@users.noreply.github.com>
2020-08-07 16:48:18 +0100

    Add node version to swagger ui

--- BlockBase.Node/ApiWebHostBuilder.cs
+++ BlockBase.Node/ApiWebHostBuilder.cs
@@ -31,6 +31,7 @@ using BlockBase.Runtime.Provider.AutomaticProduction;
 using Serilog.Events;
 using System.Linq;
 using BlockBase.Node.Commands.Requester;
+using System.Reflection;
 
 namespace BlockBase.Api
 {
@@ -103,9 +104,9 @@ namespace BlockBase.Api
                 var node = configuration.GetSection("NodeConfigurations").GetValue<string>("AccountName");
                 services.AddSwaggerGen(c =>
                 {
-                    c.SwaggerDoc("providerApi", new OpenApiInfo { Title = "Service Provider API", Version = "v0.1", Description = $"Node Account: {node}" });
-                    c.SwaggerDoc("requesterApi", new OpenApiInfo { Title = "Service Requester API", Version = "v0.1", Description = $"Node Account: {node}" });
-                    c.SwaggerDoc("networkApi", new OpenApiInfo { Title = "Network API", Version = "v0.1", Description = $"Node Account: {node}" });
+                    c.SwaggerDoc("providerApi", new OpenApiInfo { Title = "Service Provider API", Version = "v0.1", Description = $"Node Version: {Assembly.GetEntryAssembly().GetName().Version.ToString()} | Node Account: {node}" });
+                    c.SwaggerDoc("requesterApi", new OpenApiInfo { Title = "Service Requester API", Version = "v0.1", Description = $"Node Version: {Assembly.GetEntryAssembly().GetName().Version.ToString()} | Node Account: {node}" });
+                    c.SwaggerDoc("networkApi", new OpenApiInfo { Title = "Network API", Version = "v0.1", Description = $"Node Version: {Assembly.GetEntryAssembly().GetName().Version.ToString()} | Node Account: {node}" });
                     var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
                     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                     c.IncludeXmlComments(xmlPath);
