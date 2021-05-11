9670257ebf5c4941d7893f91be1c670173e81521
LeGoonie <bruno.goonie@hotmail.com>
2021-05-11 12:31:07 +0100

    Pull from dev, bug in producerstake dropdown menu

--- BlockBase.Node/Commands/Provider/GetProducingSidechainsCommand.cs
+++ BlockBase.Node/Commands/Provider/GetProducingSidechainsCommand.cs
@@ -53,27 +53,24 @@ namespace BlockBase.Node.Commands.Provider
                     var state = await _mainchainService.RetrieveContractState(sidechain.SidechainPool.ClientAccountName);
                     var providerState = candidates.Any(c => c.Key == _nodeConfigurations.AccountName) ? "Candidate" :
                                         producers.Any(p => p.Key == _nodeConfigurations.AccountName) ? "Producer" :
-                                        "Provider not found in sidechain candidates or producers";
-
-                    var producingSidechain = new ProducingSidechain()
-                    {
-                        Name = sidechain.SidechainPool.ClientAccountName,
-                        ProviderState = providerState,
-                        SidechainState = GetSidechainState(state),
-                        BlocksProducedInCurrentSettlement = Convert.ToInt32(blocksCount.Where(b => b.Key == _nodeConfigurations.AccountName)?.SingleOrDefault().blocksproduced),
-                        BlocksFailedInCurrentSettlement = Convert.ToInt32(blocksCount.Where(b => b.Key == _nodeConfigurations.AccountName)?.SingleOrDefault().blocksfailed)
-                    };
-
-                    foreach (var warning in warnings.Where(w => w.Producer == _nodeConfigurations.AccountName))
-                    {
-                        producingSidechain.Warnings.Add(new ProducingSidechainWarning()
+                                        "Provider not found in sidechain candidates or producers";                  
+                        var producingSidechain = new ProducingSidechain()
                         {
-                            WarningType = (WarningTypeEnum)warning.WarningType,
-                            WarningTimestamp = DateTimeOffset.FromUnixTimeSeconds((long)warning.WarningCreationDateInSeconds).DateTime
-                        });
-                    }
-
-                    producingSidechainsResult.Add(producingSidechain);
+                            Name = sidechain.SidechainPool.ClientAccountName,
+                            ProviderState = providerState,
+                            SidechainState = GetSidechainState(state),
+                            BlocksProducedInCurrentSettlement = Convert.ToInt32(blocksCount.Where(b => b.Key == _nodeConfigurations.AccountName).SingleOrDefault()?.blocksproduced),
+                            BlocksFailedInCurrentSettlement = Convert.ToInt32(blocksCount.Where(b => b.Key == _nodeConfigurations.AccountName).SingleOrDefault()?.blocksfailed)
+                        };
+                        foreach (var warning in warnings.Where(w => w.Producer == _nodeConfigurations.AccountName))
+                        {
+                            producingSidechain.Warnings.Add(new ProducingSidechainWarning()
+                            {
+                                WarningType = (WarningTypeEnum)warning.WarningType,
+                                WarningTimestamp = DateTimeOffset.FromUnixTimeSeconds((long)warning.WarningCreationDateInSeconds).DateTime
+                            });
+                        }
+                        producingSidechainsResult.Add(producingSidechain);                    
                 }
 
                 return new CommandExecutionResponse(HttpStatusCode.OK, new OperationResponse<List<ProducingSidechain>>(producingSidechainsResult, $"Get producing sidechains successful."));
